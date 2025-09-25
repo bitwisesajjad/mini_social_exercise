fb53a7e390f74cb5976316503ee89f9db465c50e
Sajjad Ghaeminejad <sajjadghaemi.n@gmail.com>
2025-09-25 17:25:54 +0300

    s

--- app.py
+++ app.py
@@ -935,7 +935,7 @@ def moderate_content(content):
     moderated_words = []
     for word in content.split():
         if word.lower() in bad_words:
-            modified_word = '*' * len(word)
+            modified_word = '****'
             moderated_words.append(modified_word)
             score = 5
         else:
